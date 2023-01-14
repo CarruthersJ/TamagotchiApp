bc1acdb367b63205a3d47e9cafde0af624d722f9
James Carruthers <carruthers.j@etoncollege.org.uk>
2023-01-14 11:21:20 +0000

    beginning of tamagotchi app for iOS

---
+++ TamagotchiApp/Preview Content/TamagotchiNameList.swift
@@ -0,0 +1,12 @@
+//
+//  TamagotchiNameList.swift
+//  TamagotchiIOSApp
+//
+//  Created by Carruthers, James (EJNR) on 14/01/2023.
+//
+
+import Foundation
+
+struct TamagotchiNames {
+    let tamagotchiNames = ["Mametchi", "Kuromametchi", "Kikitchi", "Tarakotchi", "Gozarutchi", "Nonopotchi", "Urutchi", "Shimashimatchi"]
+}
--- TamagotchiAppTests/TamagotchiAppTests.swift
+++ TamagotchiAppTests/TamagotchiAppTests.swift
@@ -6,31 +6,27 @@
 //
 
 import XCTest
-@testable import TamagotchiApp
+@testable import TamagotchiIOSApp
 
 class TamagotchiAppTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    func testThatTamagotchiInitialisesWithBaseLevelsOfAttributes() {
+        let tamagotchi = TamagotchiClass(name: "Tamagotchi")
+        let tamagotchiAttributes = tamagotchi.displayStats()
+        
+        let expectedHunger = 5
+        let expectedHappiness = 3.0
+        let expectedIsTired = false
+        let expectedName = "Tamagotchi"
+        let expectedAge = 0
+        let expectedIsSick = false
+        
+        XCTAssertEqual(tamagotchiAttributes.hunger, expectedHunger)
+        XCTAssertEqual(tamagotchiAttributes.happiness, expectedHappiness)
+        XCTAssertEqual(tamagotchiAttributes.isTired, expectedIsTired)
+        XCTAssertEqual(tamagotchiAttributes.name, expectedName)
+        XCTAssertEqual(tamagotchiAttributes.age, expectedAge)
+        XCTAssertEqual(tamagotchiAttributes.isSick, expectedIsSick)
     }
 
 }
